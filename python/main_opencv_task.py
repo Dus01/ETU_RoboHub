300a0996c1b73ba881ad46ebb0179f1e3cc0e9f3
katota <katota@gmail.com>
2024-10-22 16:01:38 +0300

    edit camera detection

--- python/main_opencv_task.py
+++ python/main_opencv_task.py
@@ -1,51 +1,64 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("assets/opencv/find_cubes.png")
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 11, 2)
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cap = cv2.VideoCapture(0)
 
 color_ranges = {
     "red": {
-        "lower": np.array([0, 0, 180]),  # Lower bound for red
-        "upper": np.array([10, 10, 255]) # Upper bound for red
+        "lower": np.array([0, 0, 180]),
+        "upper": np.array([10, 10, 255])
     },
     "green": {
-        "lower": np.array([0, 100, 0]),  # Lower bound for green
-        "upper": np.array([80, 255, 80]) # Upper bound for green
+        "lower": np.array([0, 100, 0]),
+        "upper": np.array([80, 255, 80])
     },
     "blue": {
-        "lower": np.array([100, 80, 0]),  # Lower bound for blue
-        "upper": np.array([255, 150, 50]) # Upper bound for blue
+        "lower": np.array([100, 80, 0]),
+        "upper": np.array([255, 150, 50])
     },
     "yellow": {
-        "lower": np.array([20, 100, 100]),  # Lower bound for yellow
-        "upper": np.array([50, 255, 255]) # Upper bound for yellow
+        "lower": np.array([20, 100, 100]),
+        "upper": np.array([50, 255, 255])
     },
     "purple": {
-        "lower": np.array([130, 0, 100]),  # Lower bound for purple
-        "upper": np.array([255, 50, 255]) # Upper bound for purple
+        "lower": np.array([130, 0, 100]),
+        "upper": np.array([255, 50, 255])
     },
 }
 
-for contour in contours:
-    approx = cv2.approxPolyDP(contour, 0.05 * cv2.arcLength(contour, True), True)
-    if len(approx) == 4:
-        x, y, w, h = cv2.boundingRect(approx)
-        center_x = int(x + w / 2)
-        center_y = int(y + h / 2)
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 25, 13)
+    cv2.imshow("Thresh", thresh)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        color = "unknown"
-        for name, color_range in color_ranges.items():
-            mean_color = np.mean(img[y:y + h, x:x + w], axis=(0, 1))
-            print(mean_color)
-            if np.all((mean_color >= color_range["lower"]) & (mean_color <= color_range["upper"])):
-                color = name
-                break
-        cv2.drawContours(img, [approx], -1, (0, 255, 0), 2)
-        cv2.putText(img, color, (center_x, center_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+    for contour in contours:
+        approx = cv2.approxPolyDP(contour, 0.05 * cv2.arcLength(contour, True), True)
 
-cv2.imshow("Cubes", img)
-cv2.waitKey(0)
+        if len(approx) == 4:
+            x, y, w, h = cv2.boundingRect(approx)
+            aspect_ratio = float(w) / h
+            print(aspect_ratio)
+
+            if 0.9 <= aspect_ratio <= 1.1:  
+                center_x = int(x + w / 2)
+                center_y = int(y + h / 2)
+
+                color = "unknown"
+                mean_color = np.mean(img[y:y + h, x:x + w], axis=(0, 1))
+
+                for name, color_range in color_ranges.items():
+                    if np.all((mean_color >= color_range["lower"]) & (mean_color <= color_range["upper"])):
+                        color = name
+                        break
+
+                cv2.drawContours(img, [approx], -1, (0, 255, 0), 2)
+                cv2.putText(img, color, (center_x, center_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+
+    cv2.imshow("Cubes", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
 cv2.destroyAllWindows()
